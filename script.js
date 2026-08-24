3a6904778024e87a48c032df52279c39430686be
CatalinaS4L <catalina.lopez@s4learning.com>
2026-08-24 21:25:13 +0200

    even more back

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-//Template month:
-//  abril: `${proxy}${encodeURIComponent(`ENLACE_AQUI&_cb=${timestamp}`)}`
-
 // ==========================================
-// 1. CONFIGURACIÓN DE ENLACES POR MES
+// 1. CONFIGURACIÓN DE ENLACES POR MES (Anti-Caché)
 // ==========================================
 const timestamp = new Date().getTime();
-const proxy = "https://corsproxy.io/?";
 
 const MONTH_URLS = {
-  febrero: `${proxy}${encodeURIComponent(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRlckyPnPqEGlq9J9wk_1HwxkfHQqt6X4wHxNtPpRg-RRATO3asLAigUxUyin9D1OS0joXIpJkG8-tL/pub?gid=0&single=true&output=csv&_cb=${timestamp}`)}`,
-  marzo: `${proxy}${encodeURIComponent(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRlckyPnPqEGlq9J9wk_1HwxkfHQqt6X4wHxNtPpRg-RRATO3asLAigUxUyin9D1OS0joXIpJkG8-tL/pub?gid=397555912&single=true&output=csv&_cb=${timestamp}`)}`,
-  abril: `${proxy}${encodeURIComponent(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRlckyPnPqEGlq9J9wk_1HwxkfHQqt6X4wHxNtPpRg-RRATO3asLAigUxUyin9D1OS0joXIpJkG8-tL/pub?gid=1499336465&single=true&output=csv&_cb=${timestamp}`)}`
+  febrero: `https://docs.google.com/spreadsheets/d/e/2PACX-1vRlckyPnPqEGlq9J9wk_1HwxkfHQqt6X4wHxNtPpRg-RRATO3asLAigUxUyin9D1OS0joXIpJkG8-tL/pub?gid=0&single=true&output=csv&_cb=${timestamp}`,
+  marzo: `https://docs.google.com/spreadsheets/d/e/2PACX-1vRlckyPnPqEGlq9J9wk_1HwxkfHQqt6X4wHxNtPpRg-RRATO3asLAigUxUyin9D1OS0joXIpJkG8-tL/pub?gid=397555912&single=true&output=csv&_cb=${timestamp}`
 };
 
 let allMonthsData = {};
